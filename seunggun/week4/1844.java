3934041efac3389b630490770b8a2da53fe29133
SeungGun <saverrw2864@naver.com>
2023-10-03 19:12:36 +0900

    optimize 1844

--- seunggun/week4/1844.java
+++ seunggun/week4/1844.java
@@ -5,13 +5,11 @@ class Solution {
     private int[] dx = {0, 1, 0, -1};
     private int[] dy = {1, 0, -1, 0};
     public int solution(int[][] maps) {
-        int[][] result = new int[maps.length][maps[0].length];
         int answer = Integer.MAX_VALUE;
 
         Queue<Point> queue = new LinkedList<>(); // BFS 탐색을 위한 큐 정의
         queue.add(new Point(0, 0));
 
-        result[0][0] = 1;
         // BFS 시작
         while (!queue.isEmpty()) {
             Point c = queue.poll();
@@ -19,8 +17,8 @@ class Solution {
             // 현재 칸이 상대방 진영이면
             if (c.x == maps[0].length - 1 && c.y == maps.length - 1) {
                 // 현재 최솟값과 지금까지 지나온 길에 대한 칸 개수 비교
-                if (answer > result[c.y][c.x])
-                    answer = result[c.y][c.x];
+                if (answer > maps[c.y][c.x])
+                    answer = maps[c.y][c.x];
                 break;
             }
 
@@ -31,10 +29,9 @@ class Solution {
                 // 이동 가능한 범위면
                 if (tx >= 0 && tx < maps[0].length && ty >= 0 && ty < maps.length) {
                     // 벽이 아니고 방문한 칸이 아니라면
-                    if (maps[ty][tx] > 0) {
+                    if (maps[ty][tx] == 1) {
                         queue.add(new Point(tx, ty)); // 다음 너비 탐색을 위해 큐에 추가
-                        result[ty][tx] = result[c.y][c.x] + 1; // 이전 경로에서 1 추가
-                        maps[ty][tx] = -1; // 방문 처리
+                        maps[ty][tx] = maps[c.y][c.x] + 1; // 이전 경로에서 1 추가
                     }
                 }
             }
